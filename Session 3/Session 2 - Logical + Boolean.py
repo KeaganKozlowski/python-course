b51645baec99855efefe131e978e5953728f90b5
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-04-26 17:45:14 +0100

    Add files via upload

---
+++ Session 3/Session 2 - Logical + Boolean.py
@@ -0,0 +1,12 @@
+#Variable Declaration
+is_sunny = True
+is_raining = False
+#Conditional Statements
+if is_sunny and is_raining:
+    print("Stay inside")
+elif is_sunny:
+    print("Wear sunglasses")
+elif is_raining:
+    print("Take and umbrella")
+else:
+    print("Check the weather forecast")
