bab292e9cc0798452e36da0245ff1e156b6c7ae4
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-04-29 21:20:29 +0100

    Add files via upload

---
+++ Session 3/Floor for zero.py
@@ -0,0 +1,7 @@
+#Outputting the floors in a house
+floors = ['ground floor','first floor',
+          'second floor', 'third floor',
+          'Attic']
+print("The floor is:",floors.index('ground floor'))
+print("The floor is:",floors.index('third floor',))
+
