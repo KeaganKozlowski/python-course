4f796adc39092f962b9e2c312b766eb735c53311
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-05-08 21:41:37 +0100

    Add files via upload

---
+++ Session 4/Resources/Functions/Calling.py
@@ -0,0 +1,12 @@
+#Defining the function
+def calling(number):
+    #The argument does not need
+    #same name as variable
+    #passed in
+    return number
+
+#Works similar to print
+print(calling(12))
+#Where argument is what you want
+#to pass into function
+print(5)
---
+++ Session 4/Resources/Functions/Greeting.py
@@ -0,0 +1,7 @@
+def Greeting(name):
+    print("Hello",name,"\nHow are you doing?")
+    
+Greeting("Pam")
+Greeting("George")
+Greeting("Michael")
+Greeting(5)
