d6593a0e1ac73ffd6e8ea4522c19b2438ad37fb9
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-05-08 21:40:47 +0100

    Delete Session 4/Print.py

--- Session 4/Print.py
+++
@@ -1,9 +0,0 @@
-#Function name is print
-print("This is your prompt")
-#When you between the brackets
-#is your argument
-
-#what the definition might look
-def Print(output):
-    return output
-    
