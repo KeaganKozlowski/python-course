54ff1432b45bf751e1691a62277b855bfe1f8a11
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-05-08 21:40:02 +0100

    Delete Session 4/TriangleArea.py

--- Session 4/TriangleArea.py
+++
@@ -1,8 +0,0 @@
-def Triangle_Area(height,width):
-    area = 0.5 * height * width
-    return area
-
-print(Triangle_Area(1,2))
-print(Triangle_Area(7,9))
-print(Triangle_Area(12,45))
-print(Triangle_Area(90,712))
