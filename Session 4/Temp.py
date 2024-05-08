2470be1b4dd13e8bd2fee99f9a709a0d19d23b9a
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-05-08 21:38:10 +0100

    Add files via upload

---
+++ Session 4/Temp.py
@@ -0,0 +1,10 @@
+def Temp_Conv(Celsius):
+    print("Converting between C to F")
+    Fahr = (Celsius * 1.8) + 32
+    print(Celsius,"C is equal to",Fahr,"F")
+
+Temp_Conv(0)
+Temp_Conv(15)
+Temp_Conv(30)
+Temp_Conv(100)
+    
