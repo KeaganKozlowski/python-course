d9a7391b78ec05a192f51aea90d05a541603a0db
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2024-05-15 11:48:23 +0100

    Add files via upload

---
+++ Session 5/Session 1 Recap/IO.py
@@ -0,0 +1,2 @@
+x = input("Please enter a word")
+print("This is a word:",x)
---
+++ Session 5/Session 1 Recap/Types.py
@@ -0,0 +1,7 @@
+Integers = 5
+Floats = 5.0
+Strings = "Word"
+Booleans = True
+Lists = [5,10,15]
+Tuples = (1,2.5,5,"orange")
+Dictionaries = {"name":"John","age":18}
